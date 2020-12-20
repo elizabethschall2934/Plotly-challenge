0e448628f8132ab85f38dd4403fc9ffe7bffcf93
elizabethschall2934 <elizabeth.schall2934@gmail.com>
2020-12-20 16:03:28 -0700

    app.js file updated with read me instructions to follow

---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,20 @@
+// 1. Use the D3 library to read in `samples.json`.
+// 2. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
+// * Use `sample_values` as the values for the bar chart.
+// * Use `otu_ids` as the labels for the bar chart.
+// * Use `otu_labels` as the hovertext for the chart.
+// 3. Create a bubble chart that displays each sample.
+// * Use `otu_ids` for the x values.
+// * Use `sample_values` for the y values.
+// * Use `sample_values` for the marker size.
+// * Use `otu_ids` for the marker colors.
+// * Use `otu_labels` for the text values.
+// 4. Display the sample metadata, i.e., an individual's demographic information.
+// 5. Display each key-value pair from the metadata JSON object somewhere on the page.
+// 6. Update all of the plots any time that a new sample is selected.
+// ## Deployment
+// * Deploy your app to a free static page hosting service, such as GitHub Pages. Submit the links 
+//to your deployment and your GitHub repo.
+// ## Hints
+// * Use `console.log` inside of your JavaScript code to see what your data looks like at each step.
+// * Refer to the [Plotly.js documentation] when building the plots.
