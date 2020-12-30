8c46437d992844583ac79985015a86dd7f7bdabe
elizabethschall2934 <elizabeth.schall2934@gmail.com>
2020-12-29 21:47:12 -0700

    I can not believe I got the bonus to workgit add .

--- StarterCode/app.js
+++ StarterCode/app.js
@@ -5,6 +5,56 @@ function buildDemos(test) {
         
         var testData = demographics.filter(testObj => testObj.id == test);
         var filteredData = testData[0];
+
+//Bonus Gauge        
+        var trace3 = {
+                    type: "pie",
+                    showlegend: false,
+                    hole: 0.4,
+                    rotation: 90,
+                    values: [10, 10, 10, 10, 10, 10, 10, 10, 10, 90],
+                    text: ["0-1", "1-2", "2-3", "3-4", "4-5", "5-6", "6-7", "7-8", "8-9", ""],
+                    direction: "clockwise",
+                    textinfo: "text",
+                    textposition: "inside",
+                    marker: {
+                      colors: ["rgba(255,25,140,1.0)", "rgba(255,25,179,1.0)", "rgba(255,25,217,1.0)", "rgba(255,25,255,1.0)", "rgba(217, 25, 255, 1.0)", "rgba(179,25,255,1.0)", "rgba(140, 25, 255, 0.8)", "rgba(102,25,255,1.0)", "rgba(64, 25, 255  , 0.8)", "white"],  
+                      labels: ["0-1", "1-2", "2-3", "3-4", "4-5", "5-6", "6-7", "7-8", "8-9", " "],
+                      hoverinfo: "labels"
+                    }
+                  };
+                  var scrubsWeek = testData[0].wfreq;  
+          
+                  var degrees = 10 * scrubsWeek, radius = 0.25
+                  var radians = degrees * Math.PI / 94
+                  var x = 0.5 - (radius * Math.cos(radians))
+                  var y = 0.5 + (radius * Math.sin(radians))
+                          
+                  var layout3 = {
+                    shapes:[{
+                        type: 'line',
+                        x0: 0.5,
+                        y0: 0.5,
+                        x1: x,
+                        y1: y,
+                        line: {
+                          color: 'purple',
+                          width: 8
+                        }
+                      }],
+                    title: 'Belly Button Washing Frequency',
+                    font: {
+                      family: 'sans-serif, monospace',
+                      size: 12,
+                      color: '#9f0fd8'
+                    },
+                    xaxis: {visible: false, range: [-1, 1]},
+                    yaxis: {visible: false, range: [-1, 1]}
+                    };
+                    
+                  var data3 = [trace3];
+                  
+                  Plotly.newPlot("gauge", data3, layout3);
         
         var PANEL = d3.select("#sample-metadata");
         PANEL.html("");
@@ -15,17 +65,17 @@ function buildDemos(test) {
 });
 }
 
-function plotSubject(test) {
+function plotSubject(tests) {
     d3.json("samples.json").then((data) => {
         
         var subject = data.samples;
         
-        var testData = subject.filter(testObj => testObj.id == test);
-        var filteredData = testData[0];
+        var testsData = subject.filter(testsObj => testsObj.id == tests);
+        var filtersData = testsData[0];
         
-        var otuIds = filteredData.otu_ids;
-        var otuLabels = filteredData.otu_labels;
-        var sampleValues = filteredData.sample_values;
+        var otuIds = filtersData.otu_ids;
+        var otuLabels = filtersData.otu_labels;
+        var sampleValues = filtersData.sample_values;
 
 // 2. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
 // * Use `sample_values` as the values for the bar chart.
@@ -100,53 +150,7 @@ function plotSubject(test) {
           
           Plotly.newPlot("bubble", data2, layout2);
 
-//Bonus Gauge
-        var trace3 = {
-          type: "pie",
-          showlegend: false,
-          hole: 0.4,
-          rotation: 90,
-          values: [90 / 9, 90 / 9, 90 / 9, 90 / 9, 90 / 9, 90 / 9, 90 / 9, 90 / 9, 90 / 9, 90],
-          text: ["0-1", "1-2", "2-3", "3-4", "4-5", "5-6", "6-7", "7-8", "8-9", ""],
-          direction: "clockwise",
-          textinfo: "text",
-          textposition: "inside",
-          marker: {
-            colors: ["rgba(255,25,140,1.0)", "rgba(255,25,179,1.0)", "rgba(255,25,217,1.0)", "rgba(255,25,255,1.0)", "rgba(217, 25, 255, 0.8)", "rgba(179,25,255,1.0)", "rgba(140, 25, 255, 0.8)", "rgba(102,25,255,1.0)", "rgba(64, 25, 255  , 0.8)", "white"]
-          },  
-          labels: ["0-1", "1-2", "2-3", "3-4", "4-5", "5-6", "6-7", "7-8", "8-9", ""],
-          hoverinfo: "label"
-        };
-            
-          
-        var degrees = 180, radius = .6;
-        var radians = degrees * Math.PI / 180;
-        var x = -1 * radius * Math.cos(radians);
-        var y = radius * Math.sin(radians);
-                
-        var layout3 = {
-          shapes:[{
-              type: 'line',
-              x0: 0,
-              y0: 0,
-              x1: x,
-              y1: 0.5,
-              line: {
-                color: 'black',
-                width: 8
-              }
-            }],
-          title: 'Belly Button Washing Frequency',
-          xaxis: {visible: false, range: [-1, 1]},
-          yaxis: {visible: false, range: [-1, 1]}
-          };
-          
-          var data3 = [trace3];
-                    
-          Plotly.newPlot("gauge", data3, layout3);
-                  
-        });
-        
+        })
       }
 
 // 4. Display the sample metadata, i.e., an individual's demographic information.
